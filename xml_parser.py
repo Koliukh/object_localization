94b22d5e107cc6d24ef00bc2156b52f742af22ac
Koliukh <koliyh@mail.ru>
2018-06-09 18:36:16 +0300

    xml

--- xml_parser.py
+++ xml_parser.py
@@ -1,10 +1,11 @@
 import xml.etree.ElementTree as ET
 import os
+import numpy as np
 import glob
 path = 'C:/ML/avito/unlabeled'
 
 with open('custom_labeled.csv','w') as outfile:
-
+    outfile.write("{}\n".format('image_name,x1,y1,x2,y2'))
     for infile in glob.glob( os.path.join(path, '*.xml') ):
         tree = ET.parse(infile)
         root = tree.getroot()
@@ -18,20 +19,20 @@ with open('custom_labeled.csv','w') as outfile:
             #  print(height.text)
             depth = elem.find('depth')
             #   print(depth.text)
-            if width is not None:
-                outfile.write("{},".format(width.text))
+        np.set_printoptions(precision=3)
+
         for elem in root.findall('object'):
             bndbox = elem.find('bndbox')
             xmin = bndbox.find('xmin')
             if xmin is not None:
-                outfile.write("{},".format(xmin.text))
-                ymin = bndbox.find('ymin')
-                if ymin is not None:
-                    outfile.write("{},".format(ymin.text))
-                xmax = bndbox.find('xmax')
-                if xmax is not None:
-                    outfile.write("{},".format(xmax.text))
-                ymax = bndbox.find('ymax')
-                if ymax is not None:
-                    outfile.write("{}\n".format(ymax.text))
+                outfile.write("{:.4f},".format(int(xmin.text)/int(width.text)))
+            ymin = bndbox.find('ymin')
+            if ymin is not None:
+                outfile.write("{:.4f},".format(int(ymin.text)/int(height.text)))
+            xmax = bndbox.find('xmax')
+            if xmax is not None:
+                outfile.write("{:.4f},".format(int(xmax.text)/int(width.text)))
+            ymax = bndbox.find('ymax')
+            if ymax is not None:
+                outfile.write("{:.4f}\n".format(int(ymax.text)/int(height.text)))
                         
